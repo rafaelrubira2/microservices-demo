cdd631d2a56bb31fbbbe0d9916cbdb0669aa0dba
rafaelrubira2 <rafaelrubira2@gmail.com>
2022-11-10 22:23:32 -0300

    ^

---
+++ build.sh
@@ -0,0 +1,11 @@
+docker build ./src/loadgenerator -t loadgenerator
+docker build ./src/checkoutservice -t checkoutservice
+docker build ./src/emailservice -t emailservice
+docker build ./src/productcatalogservice -t productcatalogservice
+docker build ./src/recommendationservice -t recommendationservice
+docker build ./src/shippingservice -t shippingservice
+docker build ./src/cartservice -t cartservice
+docker build ./src/paymentservice -t paymentservice
+docker build ./src/currencyservice -t currencyservice
+docker build ./src/frontend -t frontend
+docker build ./src/adservice -t adservice
